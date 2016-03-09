c2a97377ebbb3ecde1ce3ba76caddcd9f7c692ed
Caroline Courtney <caroline@carolinecourtney.com>
2016-03-09 02:33:30 -0500

    User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/about'
   root 'welcome#index'
-  
+
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
+
 end
